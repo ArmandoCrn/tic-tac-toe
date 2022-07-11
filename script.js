b8027e492db752128af1df6848c997505bf6a462
Armando <hello.armandocrn@gmail.com>
2022-07-11 17:21:50 +0200

    Fix Css font-size for X and O

--- script.js
+++ script.js
@@ -1,28 +1,69 @@
-/*
-IPOTETICAMENTE dovessi fare il pc, cosa che non accadrà
-visto che farò i due user che giocano fra di loro
+/**
+ * Array di podizioni che contine solo i numeri di dove la x posiziona
+ * e dove la y posiziona
+ * quindi saranno
+ * const PlayerX = []
+ * const PlayerO = []
+ *
+ * potranno andare da un minimo di 0 a 8
+ *
+ * 0 1 2
+ * 3 4 5
+ * 6 7 8
+ *
+ * crea un array che contine tutte le possibili triplette di numeri per decidere
+ * il vincitore
+ *
+ * crea una variabile isDone = false e diventa true quando finisce il gioco
+ * crea una variabile turn = 1
+ * turn ++ ad ogni click
+ *
+ * if turn === 9 è un pareggio e quindi succedono le cose del pareggio
+ */
 
-
-Come settare il pc
-
-due modi:
-
-1. semplice
-facendo scegliere randomicamente un numero da x a y
-e mettendo sulla playboard quel numero
-
-2. impossibile
-determinando dove andare ad inserire il segno rispetto a dove l'utente lo ha inserito
-praticamente è come in carta sasso forbici andare sempre a far perdere
-l'user
-
-se l'user sceglie carta il pc darà sempre forbici e così via
-stessa cosa qui
-rispetto a dove l'user mette il segno, l'altro verrà messa in una posizione
-per impedire sempre di vincere
-
-e poi quando comincia il pc farà sempre la prima mossa in modo perfetto per cercare
-di vincere e continuerà così, dato che l'user dovrà essere colui che cerca di impedire
-al pc di vincere
-
-*/
+/**
+ * Quando si preme una casella cosa succede
+ * isOver = false;
+ *
+ * turn = 0
+ *
+ * una funzione andrà a registrare il numero della casella nell'array del giocatore x
+ *
+ * turn++
+ *
+ * poi una funzione andrà a mettere a schermo la x
+ * e dovrà anche mettere la classe x a schermo per far diventare il colore del testo rosso
+ * grazie al this.data.index/e.target.data.index (?) avremo il numero di dove dovremo
+ * far mettere la X a schermo
+ *
+ * poi tocca al O
+ * funzione che fa cambiare il player turn a schermo
+ * e anche la class in x ad o e viceversa
+ *
+ * si preme e succede grosso modo la stessa cosa di prima
+ * solo che anzichè mettere roba nell'array del player x andanno nel player O
+ * e dovrà anche mettere la classe o a schermo per far diventare il colore del testo blu
+ *
+ *
+ * andremo poi sempre a prendere l'index e far comparire il O
+ *
+ * turn++
+ *
+ * quando il turn > 4
+ * inizia la verifica del vincitore
+ * questa viene effettuata cercando se l'array del player x o y hanno i tre numeri
+ * per fare tris
+ * 
+ * FIXME: 
+ * const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+ *
+ */
