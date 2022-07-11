c6a4431c1675e2ed1fcb6c098abb719067e11396
Armando <hello.armandocrn@gmail.com>
2022-07-11 15:08:45 +0200

    Add color

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+IPOTETICAMENTE dovessi fare il pc, cosa che non accadrà
+visto che farò i due user che giocano fra di loro
+
+
+Come settare il pc
+
+due modi:
+
+1. semplice
+facendo scegliere randomicamente un numero da x a y
+e mettendo sulla playboard quel numero
+
+2. impossibile
+determinando dove andare ad inserire il segno rispetto a dove l'utente lo ha inserito
+praticamente è come in carta sasso forbici andare sempre a far perdere
+l'user
+
+se l'user sceglie carta il pc darà sempre forbici e così via
+stessa cosa qui
+rispetto a dove l'user mette il segno, l'altro verrà messa in una posizione
+per impedire sempre di vincere
+
+e poi quando comincia il pc farà sempre la prima mossa in modo perfetto per cercare
+di vincere e continuerà così, dato che l'user dovrà essere colui che cerca di impedire
+al pc di vincere
+
+*/
